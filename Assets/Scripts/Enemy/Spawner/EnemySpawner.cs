e38db21acafd5c228643e22516e74fd1ef038d7e
Temporas <kys76123456@gmail.com>
2022-11-01 15:27:50 +0100

    File name correction

--- Assets/Scripts/Enemy/Spawner/EnemySpawner.cs
+++ Assets/Scripts/Enemy/Spawner/EnemySpawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class EnemySpanwer : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject EnemyToSpawn;
     [SerializeField] private float TimeBetweenSpawn;
