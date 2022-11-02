69ac0e5375be914022f62ff68ef546cd8fde46f8
Lionel <li.janin@laposte.net>
2022-11-02 10:46:39 +0100

    Added simple player rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using ScriptableObjects.Variables;
 using Unity.Netcode;
 using UnityEngine;
@@ -33,12 +34,17 @@ public class PlayerMovement : NetworkBehaviour
 	
 	private CharacterController _controller;
 	private GameObject _mainCamera;
-	
-	// Start is called before the first frame update
-	void Start()
+
+	private void Awake()
 	{
 		if (Camera.main != null) _mainCamera = Camera.main.gameObject;
 		_controller = GetComponent<CharacterController>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
 	}
 
 	// Update is called once per frame
@@ -52,6 +58,9 @@ public class PlayerMovement : NetworkBehaviour
 
 	private void Move()
 	{
+		if (_horizontalAxis == Vector3.zero)
+			return;
+		
 		// set target speed based on move speed, sprint speed and if sprint is pressed
 		float targetSpeed = _walkSpeed.value;
 
@@ -63,20 +72,14 @@ public class PlayerMovement : NetworkBehaviour
 
 		_speed = targetSpeed;
 		
-		
 		//normalise input direction
 		Vector3 inputDirection = _horizontalAxis.normalized;
 
 		// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 		// if there is a move input rotate player when the player is moving
-		if (_horizontalAxis != Vector3.zero)
-		{
-			_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
-			float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, _rotateSpeed);
-
-			// rotate to face input direction relative to camera position
-			transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-		}
+		
+		_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+		
 
 		Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
