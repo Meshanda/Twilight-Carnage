de24518b603e22b8b9a13d26a5405854136693c5
unknown <kperez@gamingcampus.local>
2022-11-03 16:50:13 +0100

    player hp bar

--- Assets/Scripts/Player/PlayerTargetShoot.cs
+++ Assets/Scripts/Player/PlayerTargetShoot.cs
@@ -15,6 +15,7 @@ public class PlayerTargetShoot : NetworkBehaviour
     [Range(0.0f, 0.3f)]
     [Tooltip("How fast the character face the direction")]
     [SerializeField] private float _rotateSpeed;
+    [SerializeField] private GameObject _rotateFrom;
 
 
     private Vector3 _playerLookAt;
@@ -73,7 +74,7 @@ public class PlayerTargetShoot : NetworkBehaviour
         _targetRotation = Mathf.Atan2(_playerLookAt.x, _playerLookAt.z) * Mathf.Rad2Deg + _camera.transform.eulerAngles.y;
         float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, _rotateSpeed);
         
-        transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        _rotateFrom.gameObject.transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
     }
     
     protected void OnTargetMouse(InputValue value)
@@ -133,7 +134,7 @@ public class PlayerTargetShoot : NetworkBehaviour
             position += transform.right * (i * _bulletXOffset);
             
             Transform spawnedObject = Instantiate(_bulletPrefab, position, Quaternion.identity);
-            spawnedObject.transform.forward = transform.forward;
+            spawnedObject.transform.forward = _rotateFrom.gameObject.transform.forward;
             spawnedObject.GetComponent<NetworkObject>().Spawn(true);
             
 
