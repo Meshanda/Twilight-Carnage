49c88d45e08d2f203039abaf94ebef68d22529e5
Temporas <kys76123456@gmail.com>
2022-11-04 10:44:05 +0100

    second asset for enemy and boss spawn script

---
+++ Assets/Scripts/Enemy/ChangeSkinEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
+
+public class ChangeSkinEnemy : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _skinList;
+
+    private int _randomSkinChosen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomSkinChosen = Random.Range(0, _skinList.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
